3da1a8f1e6cb11985c291da12108bd07a8dd5061
Lone_Soul <lone_soul@live.com>
2023-09-30 18:32:22 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,7 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_LAYER_EXTRA \
-&kp ESC,           &kp Q,             &kp W,             &kp E,             &kp R,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-&kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
-&kp LCTRL,         &kp Z,             &kp X,             &kp C,             &kp V,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+&kp ESC,           &kp Q,             &kp W,             &kp F,             &kp P,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,             \
+&kp LSHFT,         &kp A,             &kp R,             &kp S,             &kp T,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,           \
+&kp LCTRL,         &kp Z,             &kp X,             &kp C,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
 U_NP,              U_NP,              &kp LALT,          &kp SPC,           &kp TAB,           &kp RET,           &kp BSPC,          &to U_BASE,        U_NP,              U_NP
